cc22c3dd54178184e93aa121554eca19013bf066
yotsuken <yotsumoto.ken@gmail.com>
2023-01-30 19:06:11 +0900

    課題提出：税込価格を計算しよう

--- src/Review01.java
+++ src/Review01.java
@@ -3,7 +3,7 @@ public class Review01 {
 
     public static void main(String[] args) {
         int price = 1500; //変数に1500を代入
-        int tax = 10; //変数に税率を代入
+        int tax = 10; //変数に税率10を代入
         int result = includingTax(price,tax); //消費税を返す
         System.out.println(price + "円の商品の税込価格" + (price+result) +"円（消費税は"+ result +"円）です。");
     }
